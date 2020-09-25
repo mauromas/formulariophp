c85f8474b5a9c5005e3de2feeeb2ec9e00b45858
mauromas <69467141+mauromas@users.noreply.github.com>
2020-09-25 10:11:42 -0700

    Add files via upload

---
+++ correos.php
@@ -0,0 +1,15 @@
+<?php
+    $destinatario = 'mauro.mas.1997@gmail.com';
+
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
+
+    $header = "Enviado desde la página de Mauro Mas";
+    $mensajeCompleto = $mensaje . "\nAtentamente: " . $nombre;
+
+
+    mail($destinatario, $email, $mensajeCompleto, $header);
+    echo "<script>alert('correo enviado exitosamente')</script>";
+    echo "<script> setTimeout(\"location.href='index.html'\",1000)</script>";
+?>
